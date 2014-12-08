3b9ba6c32053b072b0f5671ccd8d3ab3b9395e5a
James Pants <jamespants@gmail.com>
2014-12-08 14:19:44 +0100

    first

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yellowFlyersApp
+ * @description
+ * # yellowFlyersApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yellowFlyersApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yellowFlyersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yellowFlyersApp
+ */
+angular.module('yellowFlyersApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
