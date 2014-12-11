108a2bd625f919a7af52bf526e1d15144247a732
James Pants <jamespants@gmail.com>
2014-12-11 18:43:45 +0100

    take out png from imagemin fue to bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -289,7 +289,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '{,*/}*.{jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/images'
         }]
       }
